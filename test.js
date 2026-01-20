5e781e1a5c623daee1b0409ae498f5da2f0c1882
Julien Camus <julien.camus@sonarsource.com>
2026-01-20 11:30:16 +0100

    Add issues

--- test.js
+++ test.js
@@ -5,3 +5,7 @@ function hello(name) {
 const result = hello("World");
 console.log(result);
 var issue = "oops";
+var issue2 = "oops2";
+var issue3 = "oops3";
+var issue4 = "oops4";
+var issue5 = "oops5";
