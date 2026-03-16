83df81d8f7dfb174bd9ed909566ab98ecfddeb6c
Julien Camus <julien.camus@sonarsource.com>
2026-03-16 11:19:36 +0100

    feat: add diagnostic function with command injection bug

--- test.js
+++ test.js
@@ -17,3 +17,9 @@ function getUserName(user) {
 function findUser(db, username) {
     return db.query("SELECT * FROM users WHERE name = $1", [username]);
 }
+
+// New bug: command injection
+function runDiagnostic(hostname) {
+    const { execSync } = require("child_process");
+    return execSync("ping -c 1 " + hostname).toString();
+}
