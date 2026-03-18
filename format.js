fd6a62c64d4817fae1ccd29f952edf058497dd8f
Julien Camus <julien.camus@sonarsource.com>
2026-03-18 12:13:01 +0100

    chore: simplify format helpers

--- format.js
+++ format.js
@@ -1,29 +1,3 @@
-/**
- * Simple string formatting helpers.
- */
-
-function padLeft(str, length, char = ' ') {
-  while (str.length < length) str = char + str;
-  return str;
-}
-
-function padRight(str, length, char = ' ') {
-  while (str.length < length) str = str + char;
-  return str;
-}
-
-function center(str, length, char = ' ') {
-  const pad = Math.max(0, length - str.length);
-  const left = Math.floor(pad / 2);
-  return char.repeat(left) + str + char.repeat(pad - left);
-}
-
-function repeat(str, times) {
-  return str.repeat(Math.max(0, times));
-}
-
-function truncate(str, max, suffix = '...') {
-  return str.length <= max ? str : str.slice(0, max - suffix.length) + suffix;
-}
-
-module.exports = { padLeft, padRight, center, repeat, truncate };
+/** format helpers */
+function padLeft(str, len, ch) { while (str.length < len) str = ch + str; return str; }
+module.exports = { padLeft };
