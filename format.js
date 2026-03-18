ba69bc43e6648164562184d1514b32eb96cb2595
Julien Camus <julien.camus@sonarsource.com>
2026-03-18 12:20:07 +0100

    chore: simplify format helpers v4

--- format.js
+++ format.js
@@ -1,4 +1,4 @@
-/** format helpers */
+/** format helpers v4 */
 function padLeft(str, len, ch = " ") { while (str.length < len) str = ch + str; return str; }
-function padRight(str, len, ch = " ") { while (str.length < len) str = str + ch; return str; }
-module.exports = { padLeft, padRight };
+function trim(str) { return str.trim(); }
+module.exports = { padLeft, trim };
