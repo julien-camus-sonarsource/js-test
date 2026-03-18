e9fba25759b43f7977055fa09be21cf79b896057
Julien Camus <julien.camus@sonarsource.com>
2026-03-18 12:14:18 +0100

    feat: restore padRight helper

--- format.js
+++ format.js
@@ -1,3 +1,4 @@
 /** format helpers */
-function padLeft(str, len, ch) { while (str.length < len) str = ch + str; return str; }
-module.exports = { padLeft };
+function padLeft(str, len, ch = " ") { while (str.length < len) str = ch + str; return str; }
+function padRight(str, len, ch = " ") { while (str.length < len) str = str + ch; return str; }
+module.exports = { padLeft, padRight };
